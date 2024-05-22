6ff1f4a994309480df5effd6d5fe0f9cb6f54b34
numanqamar16 <numanqamar16@gmail.com>
2024-05-22 11:51:54 +0500

    last commit

--- practice/blog website/middleware/authMiddleware.js
+++ practice/blog website/middleware/authMiddleware.js
@@ -1,7 +1,20 @@
-// Check if user is authenticated middleware
-const checkAuth = (req, res, next) => {
-    req.isAuthenticated = !!req.session.user;
-    next();
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        console.error('Error verifying token:', err);
+        res.status(401).json({ message: 'Invalid token' });
+    }
 };
 
-module.exports = checkAuth;
+module.exports = verifyToken;
